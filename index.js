b6effc3aefcdd84b960d55576187e68d74f69ca1
Heather Walker <heatherbishop022@gmail.com>
2026-09-18 18:27:46 -0500

    Complete Hello Http Server

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// write your code here
+var http = require("http");
+var server = http.createServer(function (request, response) {
+console.log(request.url);
+response.writeHead(200, { "Content-Type": "text/plain" });
+response.end("Hello World");
+});
+server.listen(8080, function () {
+   console.log("Server is listening on port 8080");
+});
